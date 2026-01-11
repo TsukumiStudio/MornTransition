25581d6b83d51b1f433896ba48294e98bb00034b
matsufriends <pikapikan0526@gmail.com>
2026-01-11 21:44:34 +0900

    ModuleNameのアクセス修飾子変更

--- MornTransitionGlobal.cs
+++ MornTransitionGlobal.cs
@@ -5,7 +5,7 @@ namespace MornLib
     [CreateAssetMenu(menuName = "Morn/" + nameof(MornTransitionGlobal), fileName = nameof(MornTransitionGlobal))]
     internal sealed class MornTransitionGlobal : MornGlobalBase<MornTransitionGlobal>
     {
-        public override string ModuleName => nameof(MornTransitionAnimation);
+        protected override string ModuleName => nameof(MornTransitionAnimation);
         [SerializeField] private string[] _transitionNames;
         public string[] TransitionNames => _transitionNames;
     }
