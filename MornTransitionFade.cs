cd33cabd2d9e8e244090521deec094851ce3d7ca
matsufriends <pikapikan0526@gmail.com>
2026-02-16 13:48:09 +0900

    Easeを設定可能に

--- MornTransitionFade.cs
+++ MornTransitionFade.cs
@@ -5,66 +5,68 @@ using UnityEngine.UI;
 
 namespace MornLib
 {
-    internal sealed class MornTransitionFade : MornTransitionBase
-    {
-        [SerializeField] private Image _image;
-        [SerializeField] private SpriteRenderer _renderer;
-        [SerializeField] private float _fillDuration;
-        [SerializeField] private float _clearDuration;
+	internal sealed class MornTransitionFade : MornTransitionBase
+	{
+		[SerializeField] private Image _image;
+		[SerializeField] private SpriteRenderer _renderer;
+		[SerializeField] private float _fillDuration;
+		[SerializeField] private MornEaseType _fillEase;
+		[SerializeField] private float _clearDuration;
+		[SerializeField] private MornEaseType _clearEase;
 
-        public override async UniTask FillAsync(CancellationToken ct = default)
-        {
-            var elapsed = 0f;
-            while (elapsed < _fillDuration)
-            {
-                elapsed += Time.deltaTime;
-                var t = Mathf.Clamp01(elapsed / _fillDuration);
-                SetAlpha(t);
-                await UniTask.Yield(PlayerLoopTiming.Update, ct);
-            }
+		public override async UniTask FillAsync(CancellationToken ct = default)
+		{
+			var elapsed = 0f;
+			while (elapsed < _fillDuration)
+			{
+				elapsed += Time.deltaTime;
+				var easedT = Mathf.Clamp01(elapsed / _fillDuration).Ease(_fillEase);
+				SetAlpha(easedT);
+				await UniTask.Yield(PlayerLoopTiming.Update, ct);
+			}
 
-            SetAlpha(1f);
-        }
+			SetAlpha(1f);
+		}
 
-        public override async UniTask ClearAsync(CancellationToken ct = default)
-        {
-            var elapsed = 0f;
-            while (elapsed < _clearDuration)
-            {
-                elapsed += Time.deltaTime;
-                var t = Mathf.Clamp01(elapsed / _clearDuration);
-                SetAlpha(1f - t);
-                await UniTask.Yield(PlayerLoopTiming.Update, ct);
-            }
+		public override async UniTask ClearAsync(CancellationToken ct = default)
+		{
+			var elapsed = 0f;
+			while (elapsed < _clearDuration)
+			{
+				elapsed += Time.deltaTime;
+				var easedT = 1f - Mathf.Clamp01(elapsed / _clearDuration).Ease(_clearEase);
+				SetAlpha(easedT);
+				await UniTask.Yield(PlayerLoopTiming.Update, ct);
+			}
 
-            SetAlpha(0f);
-        }
+			SetAlpha(0f);
+		}
 
-        public override void FillImmediate()
-        {
-            SetAlpha(1f);
-        }
+		public override void FillImmediate()
+		{
+			SetAlpha(1f);
+		}
 
-        public override void ClearImmediate()
-        {
-            SetAlpha(0f);
-        }
+		public override void ClearImmediate()
+		{
+			SetAlpha(0f);
+		}
 
-        private void SetAlpha(float alpha)
-        {
-            if (_image != null)
-            {
-                var color = _image.color;
-                color.a = alpha;
-                _image.color = color;
-            }
+		private void SetAlpha(float alpha)
+		{
+			if (_image != null)
+			{
+				var color = _image.color;
+				color.a = alpha;
+				_image.color = color;
+			}
 
-            if (_renderer != null)
-            {
-                var color = _renderer.color;
-                color.a = alpha;
-                _renderer.color = color;
-            }
-        }
-    }
+			if (_renderer != null)
+			{
+				var color = _renderer.color;
+				color.a = alpha;
+				_renderer.color = color;
+			}
+		}
+	}
 }
--- MornTransitionMaterial.cs
+++ MornTransitionMaterial.cs
@@ -5,75 +5,78 @@ using UnityEngine.UI;
 
 namespace MornLib
 {
-    internal sealed class MornTransitionMaterial : MornTransitionBase
-    {
-        [SerializeField] private Image _image;
-        [SerializeField] private string _fillRatePropertyName;
-        [SerializeField] private string _isClearPropertyName;
-        [SerializeField] private float _fillDuration;
-        [SerializeField] private float _clearDuration;
-        private Material _material;
-        private int _rateId;
-        private int _isClearId;
-        private CancellationTokenSource _cts;
+	internal sealed class MornTransitionMaterial : MornTransitionBase
+	{
+		[SerializeField] private Image _image;
+		[SerializeField] private string _fillRatePropertyName;
+		[SerializeField] private string _isClearPropertyName;
+		[SerializeField] private float _fillDuration;
+		[SerializeField] private MornEaseType _fillEase;
+		[SerializeField] private float _clearDuration;
+		[SerializeField] private MornEaseType _clearEase;
+		private Material _material;
+		private int _rateId;
+		private int _isClearId;
+		private CancellationTokenSource _cts;
 
-        private void Awake()
-        {
-            // 操作用にマテリアルを複製してImageに入れる
-            _material = new Material(_image.materialForRendering);
-            _image.material = _material;
-            _rateId = Shader.PropertyToID(_fillRatePropertyName);
-            _isClearId = Shader.PropertyToID(_isClearPropertyName);
-        }
+		private void Awake()
+		{
+			// 操作用にマテリアルを複製してImageに入れる
+			_material = new Material(_image.materialForRendering);
+			_image.material = _material;
+			_rateId = Shader.PropertyToID(_fillRatePropertyName);
+			_isClearId = Shader.PropertyToID(_isClearPropertyName);
+		}
 
-        public override async UniTask FillAsync(CancellationToken ct = default)
-        {
-            _cts?.Cancel();
-            _cts = CancellationTokenSource.CreateLinkedTokenSource(ct);
-            var start = _material.GetFloat(_rateId);
-            _material.SetFloat(_isClearId, 0);
-            await ColorTweenTask(start, 1, _fillDuration * (1 - start), _cts.Token);
-        }
+		public override async UniTask FillAsync(CancellationToken ct = default)
+		{
+			_cts?.Cancel();
+			_cts = CancellationTokenSource.CreateLinkedTokenSource(ct);
+			var start = _material.GetFloat(_rateId);
+			_material.SetFloat(_isClearId, 0);
+			await ColorTweenTask(start, 1, _fillDuration * (1 - start), _fillEase, _cts.Token);
+		}
 
-        public override async UniTask ClearAsync(CancellationToken ct = default)
-        {
-            _cts?.Cancel();
-            _cts = CancellationTokenSource.CreateLinkedTokenSource(ct);
-            var start = _material.GetFloat(_rateId);
-            _material.SetFloat(_isClearId, 1);
-            await ColorTweenTask(start, 0, _clearDuration * start, _cts.Token);
-        }
+		public override async UniTask ClearAsync(CancellationToken ct = default)
+		{
+			_cts?.Cancel();
+			_cts = CancellationTokenSource.CreateLinkedTokenSource(ct);
+			var start = _material.GetFloat(_rateId);
+			_material.SetFloat(_isClearId, 1);
+			await ColorTweenTask(start, 0, _clearDuration * start, _clearEase, _cts.Token);
+		}
 
-        public override void FillImmediate()
-        {
-            _cts?.Cancel();
-            _cts = null;
-            _material.SetFloat(_rateId, 1);
-            _material.SetFloat(_isClearId, 0);
-        }
+		public override void FillImmediate()
+		{
+			_cts?.Cancel();
+			_cts = null;
+			_material.SetFloat(_rateId, 1);
+			_material.SetFloat(_isClearId, 0);
+		}
 
-        public override void ClearImmediate()
-        {
-            _cts?.Cancel();
-            _cts = null;
-            _material.SetFloat(_rateId, 0);
-            _material.SetFloat(_isClearId, 1);
-        }
+		public override void ClearImmediate()
+		{
+			_cts?.Cancel();
+			_cts = null;
+			_material.SetFloat(_rateId, 0);
+			_material.SetFloat(_isClearId, 1);
+		}
 
-        private async UniTask ColorTweenTask(float start, float end, float duration, CancellationToken ct = default)
-        {
-            if (duration > 0)
-            {
-                var elapsedTime = 0f;
-                while (elapsedTime < duration)
-                {
-                    elapsedTime += Time.unscaledDeltaTime;
-                    _material.SetFloat(_rateId, Mathf.Lerp(start, end, elapsedTime / duration));
-                    await UniTask.Yield(ct);
-                }
-            }
+		private async UniTask ColorTweenTask(float start, float end, float duration, MornEaseType ease, CancellationToken ct = default)
+		{
+			if (duration > 0)
+			{
+				var elapsedTime = 0f;
+				while (elapsedTime < duration)
+				{
+					elapsedTime += Time.unscaledDeltaTime;
+					var easedT = Mathf.Clamp01(elapsedTime / duration).Ease(ease);
+					_material.SetFloat(_rateId, Mathf.Lerp(start, end, easedT));
+					await UniTask.Yield(ct);
+				}
+			}
 
-            _material.SetFloat(_rateId, end);
-        }
-    }
+			_material.SetFloat(_rateId, end);
+		}
+	}
 }
